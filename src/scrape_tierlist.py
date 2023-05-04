23300d78b35b07cd6de8fd7e206e12aa3ea3be68
Iqbal Khan <iqbal.52@hotmail.com>
2023-05-04 03:25:12 +0000

    hrefs found

--- src/scrape_tierlist.py
+++ src/scrape_tierlist.py
@@ -3,15 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import time
 import csv
+import time
 
 driver = webdriver.Chrome()
-driver.set_window_position(2000, 100)
 url = "https://lolalytics.com/lol/tierlist/"
-
-
-
 
 file = open("TierlistPatch13.8.csv", 'w', newline='')
 writer = csv.writer(file)
@@ -20,7 +16,7 @@ writer = csv.writer(file)
 writer.writerow(['Champion Name', 'Win rate', 'Pick Rate'])
 
 driver.get(url)
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "TierList_list__j33gd")))
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "ListRow_name__b5btO")))
 
 #List of attributes that are getting scrapped.
 name=[]
@@ -30,9 +26,7 @@ pickRate=[]
 content = driver.page_source
 soup = BeautifulSoup(content, features="html.parser")
 
-cn = ""
-r = soup.find('div', 'TierList_list__j33gd').find_all('div', recursive=False)
-for row in r:
+for row in soup.find('div', 'TierList_list__j33gd').find_all('div', recursive=False):
     columns = row.find_all('div')
     a = columns[0].text #icon?
     name = columns[1].text
@@ -44,19 +38,15 @@ for row in r:
     pr = columns[8].text #pick rate
     br = columns[9].text #ban rate
     games = columns[10].text #games
+
     #print(f'{e} - {f} - {g} - {h}')
-    cn = columns[1]['class'][0]
     writer.writerow([name, wr, pr])
 
-h = self.get_hrefs(cn)
-print(h)
-
+classname = columns[1]['class'][0]
+hrefs = driver.find_elements(By.CSS_SELECTOR, "."+classname+" a")
+for href in hrefs:
+    print(href.get_attribute("href"))
+    
 driver.close()
 file.close()
-
-def get_hrefs(cn):
-    hrefs = driver.find_elements(By.CSS_SELECTOR, "."+cn+" a")
-    for href in hrefs:
-        h = h + href.get_attribute("href") #href of each champ
-    return h    
 
